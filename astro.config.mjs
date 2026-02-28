7021578f577ad3642f35f0f2023ae8d2c3385e9c
Léo Blouin <leoblouinpro@gmail.com>
2026-02-28 15:47:59 +0100

    fix(package.json): Add sharp

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,13 @@ import tailwindcss from '@tailwindcss/vite';
 import icon from 'astro-icon';
 
 export default defineConfig({
-  // 1. Les intégrations Astro (C'est ici que vit le module virtuel)
   integrations: [icon()],
-  // 2. Les plugins Vite (Pour Tailwind 4 notamment)
   vite: {
     plugins: [tailwindcss()],
+  },
+  image: {
+    service: {
+      entrypoint: 'astro/assets/services/noop',
+    },
   },
 });
